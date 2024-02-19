b2cf5bcec730e6af9e5a312ee9280ac8f6761e2c
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2024-02-19 16:00:15 -0600

    Added slf4j logging to gradle build and logging statements to CalculationService. scaleRecipe is now retrieving and returning  all of the recipe and ingredient fields except new recipe id.

--- src/main/java/com/michaelrkaplan/bakersassistant/services/CalculationService.java
+++ src/main/java/com/michaelrkaplan/bakersassistant/services/CalculationService.java
@@ -3,13 +3,16 @@ package com.michaelrkaplan.bakersassistant.services;
 import com.michaelrkaplan.bakersassistant.models.Ingredient;
 import com.michaelrkaplan.bakersassistant.models.Recipe;
 import com.michaelrkaplan.bakersassistant.models.UnitType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import static com.michaelrkaplan.bakersassistant.services.ConversionService.convertToGrams;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CalculationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CalculationService.class);
 
     @Autowired
     private ConversionService conversionService;
@@ -29,18 +32,32 @@ public class CalculationService {
         return conversionService.convert(totalWeightInGrams, UnitType.grams, targetUnit);
     }
 
+    @Transactional
     public Recipe scaleRecipe(Recipe originalRecipe, int batchSizeMultiplier) {
         // Create a new recipe to store scaled ingredients
         Recipe scaledRecipe = new Recipe();
 
+        // Set name and instructions of scaledIngredient
+        scaledRecipe.setName(originalRecipe.getName() + " x " + batchSizeMultiplier);
+        scaledRecipe.setInstructions(originalRecipe.getInstructions());
+
         // Scale each ingredient and add to the scaled recipe
         for (Ingredient originalIngredient : originalRecipe.getIngredients()) {
             Ingredient scaledIngredient = scaleIngredient(originalIngredient, batchSizeMultiplier);
+
+            // Set the recipe of the scaled ingredient to the scaled recipe
+            scaledIngredient.setRecipe(scaledRecipe);
+
+            // Add the scaled ingredient to the scaled recipe
             scaledRecipe.addIngredient(scaledIngredient);
         }
 
+        // Logging statements
+        LOGGER.info("Scaling recipe: {} with id: {} to a new recipe with id: {}", originalRecipe.getName(), originalRecipe.getId(), scaledRecipe.getId());
+
         return scaledRecipe;
     }
+
 
     private Ingredient scaleIngredient(Ingredient originalIngredient, int batchSizeMultiplier) {
         // Create a new ingredient to store scaled details
@@ -51,7 +68,12 @@ public class CalculationService {
         double scaledQuantity = originalIngredient.getQuantity() * batchSizeMultiplier;
         scaledIngredient.setQuantity(scaledQuantity);
 
-        // You may need to handle scaling for other properties like unit, etc.
+        // Set UnityType of the ingredient
+        UnitType scaledUnit = originalIngredient.getUnit();
+        scaledIngredient.setUnit(scaledUnit);
+
+        // Logging statements
+        LOGGER.info("Scaling ingredient: {} from {} to {}", originalIngredient.getName(), originalIngredient.getQuantity(), scaledQuantity);
 
         return scaledIngredient;
     }
