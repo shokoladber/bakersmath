5f895eb6067aa596945d2d1e7276af15a36ef087
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2024-03-24 17:46:29 -0500

    Finally user can log in. I've been working on this so long. I just needed to set CustomUserDetailsImpl boolean methods from false to true. I'm dead.

--- src/main/java/com/michaelrkaplan/bakersassistant/service/CustomUserDetailsImpl.java
+++ src/main/java/com/michaelrkaplan/bakersassistant/service/CustomUserDetailsImpl.java
@@ -35,21 +35,21 @@ public class CustomUserDetailsImpl implements CustomUserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
